afb281f122e1d796dee895e9387e8ee4f04cb187
ALNoman78 <abdullaalno078@gmail.com>
2025-11-01 23:10:25 +0600

    data iteration

--- loop.js
+++ loop.js
@@ -1,9 +1,33 @@
-const startTime = performance.now()
+// const startTime = performance.now()
 
-for(let i=0; i < 5000; i++){
-    console.log(i);
+// for(let i=0; i < 5000; i++){
+//     console.log(i);
+// }
+
+// const endTime = performance.now()
+
+// console.log(`This code execuation took ${endTime - startTime} ms`)
+
+// another way to explore
+
+const firstArray = []
+const secondArray = []  
+
+for(let i=0; i < 600000; i++){
+    if( i <= 300000){
+        firstArray.push(i)
+    }
+        secondArray.push(i)
 }
 
-const endTime = performance.now()
+console.log("First array", firstArray.length)
+console.log("second array", secondArray.length)
 
-console.log()
+
+console.time("map1")
+const userFirstData = firstArray.map( number => ({ userId: number}))
+console.timeEnd("map1")
+
+console.time("map2")
+const userSecondArray = secondArray.map( (secondNumber) => ({ secUser : secondNumber}))
+console.timeEnd("map2")
