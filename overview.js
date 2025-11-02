4a392c73de8e2a693ef5205ae6bc4cece32d3957
ALNoman78 <abdullaalno078@gmail.com>
2025-11-02 18:54:46 +0600

    complete overview about big-o notation

--- overview.js
+++ overview.js
@@ -1,1 +1,54 @@
-console.log("overview js")
+    const arr = [
+    "apple",
+    "banana",
+    "orange",
+    "apple",
+    "mango",
+    "grape",
+    "pineapple",
+    "strawberry",
+    "watermelon",
+    "kiwi",
+    "pear",
+    "cherry",
+    "banana",
+    "blueberry",
+    "orange",
+    "papaya",
+    "lemon",
+    "peach",
+    "plum",
+    "apple"
+    ];
+
+    // console.log(arr.length)
+
+    // use normal array method
+
+    console.time("array")
+    const removeDuplicationArr = (arr) => {
+        const newArr = []
+        arr.forEach((element) => {
+            if(!newArr.includes(element)){
+                newArr.push(element)
+            }
+        })
+        return newArr;
+    }
+    const test = removeDuplicationArr(arr)
+    console.timeEnd("array")
+
+    // console.log(test)
+    
+    // set method
+
+    console.time("set")
+        const removeUseSet = (arr) => {
+        const set = new Set(arr);
+
+        // console.log(set)
+        return Array.from(set)
+    }
+    console.timeEnd("set")
+
+    // console.log(removeUseSet(arr))
